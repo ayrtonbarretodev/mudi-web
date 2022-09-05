9783e919972e11e11b62bb6cc5d61458625c0bb1
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-05 18:21:18 -0300

    acrescentando um paramêtro do DTO RequisicaoNovoPedido no método formulario da classe PedidoController para quando acessar o caminho /pedido/formulario, o thymeleaf conheça essa requisição através do th:object

--- src/main/java/br/com/ayrton/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/ayrton/mvc/mudi/controller/PedidoController.java
@@ -4,9 +4,12 @@ import br.com.ayrton.mvc.mudi.dto.RequisicaoNovoPedido;
 import br.com.ayrton.mvc.mudi.model.Pedido;
 import br.com.ayrton.mvc.mudi.service.PedidoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("pedido")
@@ -18,12 +21,15 @@ public class PedidoController {
     }
 
     @GetMapping("formulario")
-    public String formulario(){
+    public String formulario(RequisicaoNovoPedido requisicao){
         return "pedido/formulario";
     }
 
     @PostMapping("novo")
-    public String novo(RequisicaoNovoPedido requisicao){
+    public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result){
+        if (result.hasErrors()){
+            return "pedido/formulario";
+        }
         Pedido pedido = requisicao.toPedido();
         pedidoService.cadastrar(pedido);
         return "pedido/formulario";
