9523a19b8a47f2a6b6bc4fb59804031568205f46
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-05 18:23:10 -0300

    Adicionando a anotação @NotBlank de validação nos atributos que não podem ser vazios ou nulos

--- src/main/java/br/com/ayrton/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/ayrton/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -4,11 +4,16 @@ import br.com.ayrton.mvc.mudi.model.Pedido;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class RequisicaoNovoPedido {
+    @NotBlank
     private String nomeProduto;
+    @NotBlank
     private String urlProduto;
+    @NotBlank
     private String urlImagem;
     private String descricao;
 
