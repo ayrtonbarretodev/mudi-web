7ded2be17f3a6338620b82af9263492cda373f15
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-08 10:02:06 -0300

    adicionado o atributo status na classe de Pedido

--- src/main/java/br/com/ayrton/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/ayrton/mvc/mudi/model/Pedido.java
@@ -1,12 +1,10 @@
 package br.com.ayrton.mvc.mudi.model;
 
+import br.com.ayrton.mvc.mudi.model.enums.StatusPedido;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 @Getter
@@ -22,4 +20,6 @@ public class Pedido {
     private String urlProduto;
     private String urlImagem;
     private String descricao;
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
 }
