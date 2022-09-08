8565cd0f12293fa8be3854854561f6bcdc948272
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-08 10:03:50 -0300

    Modificado o método novo do PedidoController e criado o método porStatus na classe HomeController

--- src/main/java/br/com/ayrton/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/ayrton/mvc/mudi/controller/HomeController.java
@@ -1,14 +1,19 @@
 package br.com.ayrton.mvc.mudi.controller;
 
 import br.com.ayrton.mvc.mudi.model.Pedido;
+import br.com.ayrton.mvc.mudi.model.enums.StatusPedido;
 import br.com.ayrton.mvc.mudi.service.PedidoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
     private final PedidoService pedidoService;
 
@@ -16,10 +21,23 @@ public class HomeController {
         this.pedidoService = pedidoService;
     }
 
-    @GetMapping("/home")
+    @GetMapping
     public String home(Model model){
         List<Pedido> pedidos = pedidoService.getAll();
         model.addAttribute("pedidos",pedidos);
         return "home";
+    }
+
+    @GetMapping("/{status}")
+    public String porStatus(@PathVariable("status")String status, Model model){
+        List<Pedido> pedidos = pedidoService.buscarPorStatus(StatusPedido.valueOf(status.toUpperCase()));
+        model.addAttribute("pedidos",pedidos);
+        model.addAttribute("status",status);
+        return "home";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onError(){
+        return "redirect:/home";
     }
 }
