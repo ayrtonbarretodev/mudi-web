8214a93c659713940de787bcec12ca7925ad7cae
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-08 10:00:50 -0300

    Implementação do derived queries findByStatus

--- src/main/java/br/com/ayrton/mvc/mudi/repositories/PedidoRepository.java
+++ src/main/java/br/com/ayrton/mvc/mudi/repositories/PedidoRepository.java
@@ -1,9 +1,13 @@
 package br.com.ayrton.mvc.mudi.repositories;
 
 import br.com.ayrton.mvc.mudi.model.Pedido;
+import br.com.ayrton.mvc.mudi.model.enums.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido,Long> {
+    List<Pedido> findByStatus(StatusPedido status);
 }
