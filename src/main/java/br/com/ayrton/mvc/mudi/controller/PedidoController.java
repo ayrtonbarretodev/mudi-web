9641b23bba20972997b9fae0cd8aece6701ab0aa
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-09 12:33:17 -0300

    Pegando o nome do usuário que está logado na aplicação, buscando o usuário no banco e setando ele ao pedido

--- src/main/java/br/com/ayrton/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/ayrton/mvc/mudi/controller/PedidoController.java
@@ -2,7 +2,10 @@ package br.com.ayrton.mvc.mudi.controller;
 
 import br.com.ayrton.mvc.mudi.dto.RequisicaoNovoPedido;
 import br.com.ayrton.mvc.mudi.model.Pedido;
+import br.com.ayrton.mvc.mudi.model.User;
 import br.com.ayrton.mvc.mudi.service.PedidoService;
+import br.com.ayrton.mvc.mudi.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +18,11 @@ import javax.validation.Valid;
 @RequestMapping("pedido")
 public class PedidoController {
     private final PedidoService pedidoService;
+    private final UserService userService;
 
-    public PedidoController(PedidoService pedidoService) {
+    public PedidoController(PedidoService pedidoService, UserService userService) {
         this.pedidoService = pedidoService;
+        this.userService = userService;
     }
 
     @GetMapping("formulario")
@@ -30,7 +35,17 @@ public class PedidoController {
         if (result.hasErrors()){
             return "pedido/formulario";
         }
+
+        //Pegando o usuário que está logado p/ atribuí-lo ao pedido
+        String username =  SecurityContextHolder.getContext().getAuthentication().getName();
+
+        //Buscando o usuário no banco
+        User user = userService.buscarPorNome(username);
+
+
         Pedido pedido = requisicao.toPedido();
+        //Setando o usuário no pedido
+        pedido.setUser(user);
         pedidoService.cadastrar(pedido);
         return "redirect:/home";
     }
