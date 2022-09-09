8a58dc05b17637b4e8f4539045a76d17131af5ec
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-09 18:29:00 -0300

    Criado um Controller e um template home somente para o usuário, na pasta /home/usuario, o outro home ficará para mostrar pedidos entregues no qual todos que acessam a página sem logar possam ver

---
+++ src/main/java/br/com/ayrton/mvc/mudi/controller/UsuarioController.java
@@ -0,0 +1,43 @@
+package br.com.ayrton.mvc.mudi.controller;
+
+import br.com.ayrton.mvc.mudi.model.Pedido;
+import br.com.ayrton.mvc.mudi.model.enums.StatusPedido;
+import br.com.ayrton.mvc.mudi.service.PedidoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller("/usuario")
+public class UsuarioController {
+
+    private final PedidoService pedidoService;
+
+    public UsuarioController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @GetMapping("pedido")
+    public String home(Model model, Principal principal){
+        List<Pedido> pedidos = pedidoService.buscarPedidosPorUsuario(principal.getName());
+        model.addAttribute("pedidos",pedidos);
+        return "usuario/home";
+    }
+
+    @GetMapping("pedido/{status}")
+    public String porStatus(@PathVariable("status")String status, Model model, Principal principal){
+        List<Pedido> pedidos = pedidoService.buscarPorStatusEUsuario(StatusPedido.valueOf(status.toUpperCase()), principal.getName());
+        model.addAttribute("pedidos",pedidos);
+        model.addAttribute("status",status);
+        return "usuario/home";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onError(){
+        return "redirect:/usuario/home";
+    }
+}
