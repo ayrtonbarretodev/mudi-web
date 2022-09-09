4fdbcbccae2bc85b2df3b30d127ab2c4ece348e0
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-09 11:16:59 -0300

    Configurando JDBC Authentication

--- src/main/java/br/com/ayrton/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/ayrton/mvc/mudi/WebSecurityConfig.java
@@ -1,18 +1,27 @@
 package br.com.ayrton.mvc.mudi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -30,6 +39,26 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+//        UserDetails user =
+//                User.builder()
+//                        .username("maria")
+//                        .password(encoder.encode("maria"))
+//                        .roles("ADM")
+//                        .build();
+
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder);
+                //.withUser(user);
+    }
+
+
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user =
@@ -41,4 +70,6 @@ public class WebSecurityConfig {
 
         return new InMemoryUserDetailsManager(user);
     }
+
+
 }
