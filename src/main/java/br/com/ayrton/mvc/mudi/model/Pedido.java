6a4f869052b4d84412d50da7aa7cac7c4c86a8c1
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-13 10:33:25 -0300

    Relacionamento OneToMany com ofertas

--- src/main/java/br/com/ayrton/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/ayrton/mvc/mudi/model/Pedido.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
+
 @Getter
 @Setter
 @Entity
@@ -27,5 +29,7 @@ public class Pedido {
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
-
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pedido", fetch = FetchType.EAGER)
+    @JsonIgnore
+    private List<Oferta> ofertas;
 }
